d36a7db56948c2b5045fb5fae3465614d3eac990
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-02 19:39:04 -0300

    initial commit

---
+++ src/test/java/me/calebe_oliveira/springboot_rabbitmq_tutorial/SpringbootRabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.springboot_rabbitmq_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
